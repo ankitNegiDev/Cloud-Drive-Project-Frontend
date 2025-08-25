3cac6f8af5ad7a1324c9f47a356a9e7787e8aaa0
ankitNegiDev <ankitnegi9104@gmail.com>
2025-08-25 19:35:22 +0530

    added signup auth service function

---
+++ src/services/authService.js
@@ -0,0 +1,21 @@
+// auth service -- that will have re-usable function for signup , login , logout, and getting the details of current user.
+
+// (1) signup user
+// here userData will be sent by the caller and it will generaly contain name , email , pass , avatar url etc.
+
+import api from '../utils/axiosInstance.jsx';
+export async function signup (userData){
+    try{
+        // calling the api for signup
+        const response=await api.post('/auth/signup',userData);
+        
+        // now returning the repsosne back from where it is called (context/component)
+        return response.data;
+    }catch(error){
+        console.log("error occured in singup api calling -- check auth service");
+        
+        // now throwing error back from where it is called (context/component)
+        throw error;
+    }
+}
+
